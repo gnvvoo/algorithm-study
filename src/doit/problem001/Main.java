379a8455202184ca529831bb6a8ff0c26b2f6657
gnvvoo <t412@naver.com>
2026-04-22 14:49:59 +0900

    solve: Doit 001 숫자의 합 구하기

--- src/doit/problem001/Main.java
+++ src/doit/problem001/Main.java
@@ -1,5 +1,36 @@
 package doit.problem001;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String numArr = br.readLine();
+        char[] cNum = numArr.toCharArray();
+
+        int sum = 0;
+        for (char c : cNum) {
+            sum += c -'0';
+        }
+
+        bw.write(sum + "\n");
+        bw.flush();
+        br.close();
+        bw.close();
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        String num = sc.next();
+//        char[] arr = num.toCharArray();
+//
+//        int sum = 0;
+//        for(char cNum : arr) {
+//            sum += cNum - '0';
+//        }
+//
+//        System.out.println(sum);
+    }
 }
